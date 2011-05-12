d1a6b80532d6780b27303c55f4171565afd76025
Daniel Friesel <derf@finalrewind.org>
2011-05-12 10:15:25 +0200

    App::Pstatus::Plugin::Base documentation

--- lib/App/Pstatus/Plugin/Base.pm
+++ lib/App/Pstatus/Plugin/Base.pm
@@ -49,3 +49,72 @@ sub run {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+App::Pstatus::Plugin::Base - parent class for all pstatus plugins
+
+=head1 SYNOPSIS
+
+    use parent 'App::Pstatus::Plugin::Base';
+
+    sub check {
+        my ($self) = @_;
+
+        if (everything_ok()) {
+            return {
+                data => show_things(),
+            };
+        }
+        else {
+            die("not found\n");
+        }
+    }
+
+=head1 DESCRIPTION
+
+B<App::Pstatus::Plugin::Base> is not a plugin itself; it is meant to serve as
+a parent class for all other plugins.
+
+=head1 METHODS
+
+=over
+
+=item $plugin = App::Pstatus::Plugin::Something->new(I<%conf>);
+
+Returns a new object. A reference to I<%conf> is stored in $self->{default}.
+
+=item $plugin->run(I<$conf>)
+
+Merges $self->{default} and I<$conf> and saves the result in $self->{conf}.
+I<$conf> takes precedence; $self->{default} and I<$conf> are not touched in
+the process.
+
+If $conf{enable} is set to 0, immediately returns { skip => 1 }.
+
+It then calls the check function of App::Pstatus::Plugin::Something. If it
+fails (dies or returns undef), { ok => 0, data => $@} is returned.
+
+The hashref returned by the B<check> call is returned, with the additional key
+B<ok> set to 1. Also, if $conf{href} is set, but B<check> did not set a
+B<href> key, B<href> is set to $conf{href} with %s replaced by $conf{name}.
+
+=back
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 SEE ALSO
+
+L<pstatus>, L<App::Pstatus::Plugin>.
+
+=head1 AUTHOR
+
+Copyright (C) 2011 by Daniel Friesel E<lt>derf@finalrewind.orgE<gt>
+
+=head1 LICENSE
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
