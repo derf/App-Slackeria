41fee6f187a5b657a45c23b325d43f2964c23253
Daniel Friesel <derf@finalrewind.org>
2011-05-12 09:53:21 +0200

    App::Pstatus::Plugin documentation

--- lib/App/Pstatus/Plugin.pm
+++ lib/App/Pstatus/Plugin.pm
@@ -44,3 +44,80 @@ sub run {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+App::Pstatus::Plugin - Plugin wrapper for App::Pstatus
+
+=head1 SYNOPSIS
+
+    use App::Pstatus::Plugin;
+
+    my $plugin = App::Pstatus::Plugin->new();
+    my $result;
+
+    $plugin->load('CPAN', %cpan_default_conf);
+
+    $result->{pstatus}->{CPAN} = $plugin->run('CPAN', {
+            name => 'App-Pstatus',
+            # further pstatus-specific configuration (if needed)
+    });
+
+    # $result->{pstatus}->{CPAN} is like:
+    # {
+    #     ok => 1,
+    #     data => 'v0.1',
+    #     href => 'http://search.cpan.org/dist/App-Pstatus/'
+    # }
+
+=head1 DESCRIPTION
+
+B<App::Pstatus::Plugin> loads and executes a number of B<pstatus> plugins.  It
+also makes sure that any errors in plugins are catched and do not affect the
+code using B<App::Pstatus::Plugin>.
+
+=head1 METHODS
+
+=over
+
+=item $plugin = App::Pstatus::Plugin->new()
+
+Returns a new App::Pstatus::Plugin object.  Does not take any arguments.
+
+=item $plugin->load(I<plugin>, I<%conf>)
+
+Create an internal App::Pstatus::Plugin::I<plugin> object by using it and
+calling App::Pstatus::Plugin::I<plugin>->new(I<%conf>).  If I<plugin> does not
+exist or fails during setup, B<load> prints an error message to STDERR.
+
+=item $plugin->list()
+
+Returns an array containing the names of all loaded plugins.
+
+=item $plugin->run(I<plugin>, I<$conf_ref>)
+
+Calls the B<run> method of I<plugin>:
+$plugin_object->run(I<$conf_ref>).
+
+If I<plugin> exists and is loaded, it returns the output of the run method,
+otherwise undef.
+
+=back
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 SEE ALSO
+
+L<pstatus>, L<App::Pstatus::Plugin::Base>.
+
+=head1 AUTHOR
+
+Copyright (C) 2011 by Daniel Friesel E<lt>derf@finalrewind.orgE<gt>
+
+=head1 LICENSE
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
