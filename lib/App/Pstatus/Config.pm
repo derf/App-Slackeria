e5e8461fb9b278a698558e5ecfd013fa1903046e
Daniel Friesel <derf@finalrewind.org>
2011-05-13 11:07:08 +0200

    App::Pstatus::Config documentation

--- lib/App/Pstatus/Config.pm
+++ lib/App/Pstatus/Config.pm
@@ -62,3 +62,81 @@ sub plugins {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+App::Pstatus::Plugin - Get config values for App::Pstatus and plugins
+
+=head1 SYNOPSIS
+
+    use App::Pstatus::Config;
+
+    my $conf = App::Pstatus::Config->new();
+    for my $name ($conf->plugins()) {
+        my $plugin_conf = $conf->get('config', $name);
+        # load plugin
+    }
+    for my $project ($conf->projects()) {
+        for my $name ($conf->plugins()) {
+            my $plugin_conf = $conf->get($project, $name);
+            # run plugin
+        }
+    }
+
+=head1 DESCRIPTION
+
+B<App::Pstatus::Config> uses L<Config::Tiny> to load config files.
+
+=head1 METHODS
+
+=over
+
+=item $config = App::Pstatus::Config->new()
+
+Returns a new App::Pstatus::Config object.  Does not take any arguments.
+
+=item $config->get(I<$name>, I<$section>)
+
+Returns a hashref containing the I<section> of the config file I<name>.  If
+I<name> does not exist or does not contain I<section>, returns a reference to
+an empty hash.  If I<name> is not B<config> and I<section> does not have a
+B<name> field, sets B<name> to I<name> in the hashref.
+
+=item $config->load(I<$name>)
+
+Loads $XDG_CONFIG_HOME/pstatus/I<name> (defaulting to
+~/.config/pstatus/I<name>) and saves its content internally. If the config
+file does not exist, saves an empty hashref.
+
+$config->get automatically calls this, so there should be no need for you to
+use it.
+
+=item $config->projects()
+
+Returns an array of all projects, as listed in the B<list> key in the
+B<projects> section of the B<config> file.
+
+=item $config->plugins()
+
+Returns an array of all plugins, which is actually just a list off all
+sections in the B<config> file.
+
+=back
+
+=head1 DEPENDENCIES
+
+L<Config::Tiny>, L<File::BaseDir>.
+
+=head1 SEE ALSO
+
+L<pstatus>
+
+=head1 AUTHOR
+
+Copyright (C) 2011 by Daniel Friesel E<lt>derf@finalrewind.orgE<gt>
+
+=head1 LICENSE
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
